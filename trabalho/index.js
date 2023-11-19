071a98fcdc6f2a1b3fb2479c35342474bf02fbf8
luanbalves <88348475+luanbalves@users.noreply.github.com>
2023-11-19 16:04:53 -0300

    Correção no login
    O usuário podia acessar o /home mesmo quando não logado, agora é necessário o login.

--- trabalho/index.js
+++ trabalho/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const session = require('express-session');
+const autenticacaoController = require('./src/controllers/autenticacaoController')
 const db = require('./src/db')
 
 const app = express();
@@ -13,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use(session({
+  secret: 'secret-token',
+  name: 'sessionId',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.get('/', (req, res) => {
     res.render('login.html');  
 });
@@ -21,16 +29,18 @@ app.get('/cadastro', (req, res) => {
     res.render('cadastro.html');
 });
 
-app.get('/home', (req, res) => {
-    res.render('home.html');
+// index.js
+
+// ...
+
+// Rota protegida (requer autenticação)
+app.get('/home', autenticacaoController.verificarAutenticacao, (req, res) => {
+  res.render('home.html');
 });
 
-app.use(session({
-  secret: 'secret-token',
-  name: 'sessionId',
-  resave: false,
-  saveUninitialized: false
-}));
+// ...
+
+
 
 app.use('/', require('./src/routes/usuarioRoutes'));
 app.use('/', require('./src/routes/autenticacaoRoutes'));
